5c727f6d54e3beb5b43b8d429f25b8064466679f
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-04-13 17:19:05 -0500

    Add sign in form

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
-import Home from '../pages/Home';
-import NotFound from '../pages/NotFound';
-import '../styles/global.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../containers/Layout";
+import Login from "../containers/Login";
+import Signin from "../containers/Signin";
+import RecoveryPassword from "../containers/RecoveryPassword";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+import "../styles/global.css";
 
 const App = () => {
-    return(
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                        <Route exact path='/' element={<Home />}/>
-                        <Route exact path='/login' element={<Login />}/>
-                        <Route exact path='/recovery-password' element={<RecoveryPassword />}/>
-                        <Route exact path='*' element={<NotFound />}/>
-                </Routes>
-            </Layout>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signin" element={<Signin />} />
+          <Route
+            exact
+            path="/recovery-password"
+            element={<RecoveryPassword />}
+          />
+          <Route exact path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
