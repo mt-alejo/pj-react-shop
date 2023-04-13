678bd5cee30fdc1fd012e5d42652ce35b41b842e
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-04-13 17:29:12 -0500

    Add edit account form

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
 import Login from "../containers/Login";
 import Signin from "../containers/Signin";
+import EditAccount from "../containers/EditAccount";
 import RecoveryPassword from "../containers/RecoveryPassword";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
@@ -16,6 +17,7 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signin" element={<Signin />} />
+          <Route exact path="/edit-account" element={<EditAccount />} />
           <Route
             exact
             path="/recovery-password"
