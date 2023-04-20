3694585f80532467feac3e34b24729d54058acca
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-04-19 19:21:51 -0500

    Add: login

---
+++ src/containers/Login.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../styles/Login.scss";
+
+function Login() {
+  return (
+    <div class="login">
+      <div class="form-container">
+        <img src="./logos/logo_yard_sale.svg" alt="logo" class="logo" />
+
+        <form action="/" class="form">
+          <label for="email" class="label">
+            Email address
+          </label>
+          <input
+            type="text"
+            id="email"
+            placeholder="platzi@example.cm"
+            class="input input-email"
+          />
+
+          <label for="password" class="label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="*********"
+            class="input input-password"
+          />
+
+          <input
+            type="submit"
+            value="Log in"
+            class="primary-button login-button"
+          />
+          <a href="/">Forgot my password</a>
+        </form>
+
+        <button class="secondary-button signup-button">Sign up</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
 import NewPassword from "../containers/NewPassword";
+import RecoveryPassword from "../containers/RecoveryPassword";
+import Login from "../containers/Login";
 import Signin from "../containers/Signin";
 import EditAccount from "../containers/EditAccount";
-import RecoveryPassword from "../containers/RecoveryPassword";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import "../styles/global.css";
@@ -18,11 +19,11 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/new-password" element={<NewPassword />} />
+          <Route exact path="/login" element={<Login />} />
           <Route exact path="/signin" element={<Signin />} />
           <Route exact path="/edit-account" element={<EditAccount />} />
           <Route exact path="/user-order" element={<UserOrder />} />
           <Route exact path="/orders" element={<Orders />} />
-
           <Route
             exact
             path="/recovery-password"
