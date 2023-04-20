cc2c5a9c6466a4cd76599fd099b948e0f29dc7d3
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-04-20 17:21:29 -0500

    [Correct]: recovery-password

--- src/containers/RecoveryPassword.jsx
+++ src/containers/RecoveryPassword.jsx
@@ -9,9 +9,9 @@ const RecoveryPassword = () => {
       <div className="RecoveryPassword-container">
         <img src={logo} alt="logo" className="logo" />
 
-        <h1 className="title">Email has been sent!</h1>
+        <h1 className="RecoveryPassword-title">Email has been sent!</h1>
         <p className="subtitle">
-          Please check your inbox for instructions on how to reset the password
+          Please check your inbox for instructions on how to reset the password.
         </p>
 
         <div className="email-image">
@@ -22,7 +22,7 @@ const RecoveryPassword = () => {
 
         <p className="resend">
           <span>Didn't receive the email?</span>
-          <a href="/">Resend</a>
+          <a href="/"> Resend</a>
         </p>
       </div>
     </div>
